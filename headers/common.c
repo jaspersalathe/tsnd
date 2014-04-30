9983728bc08800936522d5fbcb88a983a6a42bb4
Jan Jasper Salathe <jaspersalathe@gmail.com>
2014-04-30 18:29:49 +0200

    ...

--- headers/common.c
+++ headers/common.c
@@ -16,9 +16,9 @@
 
 uint16_t Common_nToLu16(const uint16_t data)
 {
-#if __BYTE_ORDER__ == __BIG_ENDIAN
+#if __BYTE_ORDER == __BIG_ENDIAN
     return data;
-#elif __BYTE_ORDER__ == __LITTLE_ENDIAN
+#elif __BYTE_ORDER == __LITTLE_ENDIAN
     return ((data&0xFF) << 8) | ((data&0xFF) >> 8);
 #else
 #error unknown byteorder!
@@ -27,9 +27,9 @@ uint16_t Common_nToLu16(const uint16_t data)
 
 uint16_t Common_lToNu16(const uint16_t data)
 {
-#if __BYTE_ORDER__ == __BIG_ENDIAN
+#if __BYTE_ORDER == __BIG_ENDIAN
     return data;
-#elif __BYTE_ORDER__ == __LITTLE_ENDIAN
+#elif __BYTE_ORDER == __LITTLE_ENDIAN
     return ((data&0xFF) << 8) | ((data&0xFF) >> 8);
 #else
 #error unknown byteorder!
@@ -38,9 +38,9 @@ uint16_t Common_lToNu16(const uint16_t data)
 
 int16_t Common_nToLi16(const int16_t data)
 {
-#if __BYTE_ORDER__ == __BIG_ENDIAN
+#if __BYTE_ORDER == __BIG_ENDIAN
     return data;
-#elif __BYTE_ORDER__ == __LITTLE_ENDIAN
+#elif __BYTE_ORDER == __LITTLE_ENDIAN
     return ((data&0xFF) << 8) | ((data&0xFF) >> 8);
 #else
 #error unknown byteorder!
@@ -49,9 +49,9 @@ int16_t Common_nToLi16(const int16_t data)
 
 int16_t Common_lToNi16(const int16_t data)
 {
-#if __BYTE_ORDER__ == __BIG_ENDIAN
+#if __BYTE_ORDER == __BIG_ENDIAN
     return data;
-#elif __BYTE_ORDER__ == __LITTLE_ENDIAN
+#elif __BYTE_ORDER == __LITTLE_ENDIAN
     return ((data&0x00FF) << 8) | ((data&0xFF00) >> 8);
 #else
 #error unknown byteorder!
@@ -61,9 +61,9 @@ int16_t Common_lToNi16(const int16_t data)
 
 uint32_t Common_nToLu32(const uint32_t data)
 {
-#if __BYTE_ORDER__ == __BIG_ENDIAN
+#if __BYTE_ORDER == __BIG_ENDIAN
     return data;
-#elif __BYTE_ORDER__ == __LITTLE_ENDIAN
+#elif __BYTE_ORDER == __LITTLE_ENDIAN
     return ((data&0x000000FF) << 24) | ((data&0x0000FF00) << 8) | ((data&0x00FF0000) >> 8) | ((data&0xFF000000) >> 24);
 #else
 #error unknown byteorder!
@@ -72,9 +72,9 @@ uint32_t Common_nToLu32(const uint32_t data)
 
 uint32_t Common_lToNu32(const uint32_t data)
 {
-#if __BYTE_ORDER__ == __BIG_ENDIAN
+#if __BYTE_ORDER == __BIG_ENDIAN
     return data;
-#elif __BYTE_ORDER__ == __LITTLE_ENDIAN
+#elif __BYTE_ORDER == __LITTLE_ENDIAN
     return ((data&0x000000FF) << 24) | ((data&0x0000FF00) << 8) | ((data&0x00FF0000) >> 8) | ((data&0xFF000000) >> 24);
 #else
 #error unknown byteorder!
@@ -83,9 +83,9 @@ uint32_t Common_lToNu32(const uint32_t data)
 
 int32_t Common_nToLi32(const int32_t data)
 {
-#if __BYTE_ORDER__ == __BIG_ENDIAN
+#if __BYTE_ORDER == __BIG_ENDIAN
     return data;
-#elif __BYTE_ORDER__ == __LITTLE_ENDIAN
+#elif __BYTE_ORDER == __LITTLE_ENDIAN
     return ((data&0x000000FF) << 24) | ((data&0x0000FF00) << 8) | ((data&0x00FF0000) >> 8) | ((data&0xFF000000) >> 24);
 #else
 #error unknown byteorder!
@@ -94,9 +94,9 @@ int32_t Common_nToLi32(const int32_t data)
 
 int32_t Common_lToNi32(const int32_t data)
 {
-#if __BYTE_ORDER__ == __BIG_ENDIAN
+#if __BYTE_ORDER == __BIG_ENDIAN
     return data;
-#elif __BYTE_ORDER__ == __LITTLE_ENDIAN
+#elif __BYTE_ORDER == __LITTLE_ENDIAN
     return ((data&0x000000FF) << 24) | ((data&0x0000FF00) << 8) | ((data&0x00FF0000) >> 8) | ((data&0xFF000000) >> 24);
 #else
 #error unknown byteorder!
@@ -106,9 +106,9 @@ int32_t Common_lToNi32(const int32_t data)
 
 uint64_t Common_nToLu64(const uint64_t data)
 {
-#if __BYTE_ORDER__ == __BIG_ENDIAN
+#if __BYTE_ORDER == __BIG_ENDIAN
     return data;
-#elif __BYTE_ORDER__ == __LITTLE_ENDIAN
+#elif __BYTE_ORDER == __LITTLE_ENDIAN
     return ((data&0x00000000000000FF) << 56) | ((data&0x000000000000FF00) << 40) | ((data&0x0000000000FF0000) << 24) | ((data&0x00000000FF000000) << 8)
           | ((data&0x000000FF00000000) >>  8) | ((data&0x0000FF0000000000) << 24) | ((data&0x00FF000000000000) >> 40) | ((data&0xFF00000000000000) >> 56);
 #else
@@ -118,9 +118,9 @@ uint64_t Common_nToLu64(const uint64_t data)
 
 uint64_t Common_lToNu64(const uint64_t data)
 {
-#if __BYTE_ORDER__ == __BIG_ENDIAN
+#if __BYTE_ORDER == __BIG_ENDIAN
     return data;
-#elif __BYTE_ORDER__ == __LITTLE_ENDIAN
+#elif __BYTE_ORDER == __LITTLE_ENDIAN
     return ((data&0x00000000000000FF) << 56) | ((data&0x000000000000FF00) << 40) | ((data&0x0000000000FF0000) << 24) | ((data&0x00000000FF000000) << 8)
           | ((data&0x000000FF00000000) >>  8) | ((data&0x0000FF0000000000) << 24) | ((data&0x00FF000000000000) >> 40) | ((data&0xFF00000000000000) >> 56);
 #else
@@ -130,9 +130,9 @@ uint64_t Common_lToNu64(const uint64_t data)
 
 int64_t Common_nToLi64(const int64_t data)
 {
-#if __BYTE_ORDER__ == __BIG_ENDIAN
+#if __BYTE_ORDER == __BIG_ENDIAN
     return data;
-#elif __BYTE_ORDER__ == __LITTLE_ENDIAN
+#elif __BYTE_ORDER == __LITTLE_ENDIAN
     return ((data&0x00000000000000FF) << 56) | ((data&0x000000000000FF00) << 40) | ((data&0x0000000000FF0000) << 24) | ((data&0x00000000FF000000) << 8)
           | ((data&0x000000FF00000000) >>  8) | ((data&0x0000FF0000000000) << 24) | ((data&0x00FF000000000000) >> 40) | ((data&0xFF00000000000000) >> 56);
 #else
@@ -142,9 +142,9 @@ int64_t Common_nToLi64(const int64_t data)
 
 int64_t Common_lToNi64(const int64_t data)
 {
-#if __BYTE_ORDER__ == __BIG_ENDIAN
+#if __BYTE_ORDER == __BIG_ENDIAN
     return data;
-#elif __BYTE_ORDER__ == __LITTLE_ENDIAN
+#elif __BYTE_ORDER == __LITTLE_ENDIAN
     return ((data&0x00000000000000FF) << 56) | ((data&0x000000000000FF00) << 40) | ((data&0x0000000000FF0000) << 24) | ((data&0x00000000FF000000) << 8)
           | ((data&0x000000FF00000000) >>  8) | ((data&0x0000FF0000000000) << 24) | ((data&0x00FF000000000000) >> 40) | ((data&0xFF00000000000000) >> 56);
 #else
