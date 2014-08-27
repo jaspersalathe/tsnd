b467f196a7dc637a0f32f636b8a5d5fab3d7a21a
Jan Jasper Salathe <jaspersalathe@gmail.com>
2014-08-27 12:48:33 +0200

    Began implementation of filtering_db

--- bridge/filtering_db.c
+++ bridge/filtering_db.c
@@ -7,4 +7,201 @@
 
 #include "filtering_db.h"
 
+#include <stdlib.h>
+#include <string.h>
 
+
+static int cmpRules(const struct FDB_rule *r1, const struct FDB_rule *r2);
+
+/*
+ * Return values:
+ *             0: success
+ *            -1: pointer NULL
+ *
+ */
+int32_t FDB_init(struct FDB_state *state, struct BridgeForwarding_state *bridgeForwarding, uint32_t portCnt, struct FDB_rule *ruleMemory, uint32_t ruleAllocCnt)
+{
+    if(state == NULL || bridgeForwarding == NULL)
+        return -1;
+    if(ruleAllocCnt > 0 && ruleMemory == NULL)
+        return -1;
+
+    memset(state, 0, sizeof(struct FDB_state));
+    state->bridgeForwarding = bridgeForwarding;
+    state->portCnt = portCnt;
+    state->ruleCnt = 0;
+    state->ruleAllocCnt = ruleAllocCnt;
+    state->rules = ruleMemory;
+    state->rulesStaticallyAllocated = ruleAllocCnt > 0;
+
+    return 0;
+}
+
+/*
+ * Return values:
+ *             0: success
+ *            -1: pointer NULL
+ *            -2: rule invalid
+ *            -3: no memory left
+ */
+int32_t FDB_addRule(struct FDB_state *state, struct FDB_rule *rule)
+{
+    return 0;
+}
+
+/*
+ * Return values:
+ *             0: success
+ *            -1: pointer NULL
+ *            -2: rule not found
+ */
+int32_t FDB_delRule(struct FDB_state *state, struct FDB_rule *rule)
+{
+    uint32_t i, j;
+    int found = 0;
+
+    if(state == NULL || rule == NULL)
+        return -1;
+
+    for(i = 0, j = 0; i < state->ruleCnt; i++)
+    {
+        if(cmpRules(rule, &(state->rules[i])))
+        {
+            found = 1;
+            // del rule by not incrementing write slot and not moving it forward
+        }
+        else
+        {
+            j++;
+            if(j != i)
+            {
+                // fill up gaps
+            }
+        }
+
+    }
+
+    return found ? 0 : -2;
+}
+
+uint32_t FDB_getRuleCnt(struct FDB_state *state)
+{
+    if(state == NULL)
+        return 0;
+    return state->ruleCnt;
+}
+
+struct FDB_rule *FDB_getRuleByIdx(struct FDB_state *state, uint32_t idx)
+{
+    if(state == NULL || idx >= state->ruleCnt)
+        return NULL;
+    return &(state->rules[idx]);
+}
+
+static int cmpRules(const struct FDB_rule *r1, const struct FDB_rule *r2)
+{
+    uint32_t i;
+    const struct FDB_StaticFiltering *sf1, *sf2;
+    const struct FDB_StaticVLANRegistration *sv1, *sv2;
+    const struct FDB_DynamicFiltering *df1, *df2;
+    const struct FDB_MACAddressRegistration *ma1, *ma2;
+    const struct FDB_DynamicVLANRegistration *dv1, *dv2;
+    const struct FDB_DynamicReservation *dr1, *dr2;
+
+    if(r1 == r2)
+        return 1;
+    if(r1 == NULL || r2 == NULL)
+        return 0;
+    if(r1->type != r2->type)
+        return 0;
+
+    switch(r1->type)
+    {
+    case FDB_RuleType_StaticFiltering:
+        sf1 = &(r1->rule.staticFiltering);
+        sf2 = &(r2->rule.staticFiltering);
+        if(sf1->addrType != sf2->addrType)
+            return 0;
+        for(i = 0; i < ETHERNET_MAC_LEN; i++)
+            if(sf1->mac[i] != sf2->mac[i])
+                return 0;
+        if(sf1->portMapCnt != sf2->portMapCnt)
+            return 0;
+        if(sf1->portMap != sf2->portMap)
+            for(i = 0; i < sf1->portMapCnt; i++)
+                if(sf1->portMap[i] != sf2->portMap[i])
+                    return 0;
+        return 1;
+
+    case FDB_RuleType_StaticVLANRegistraiton:
+        sv1 = &(r1->rule.staticVLANRegistration);
+        sv2 = &(r2->rule.staticVLANRegistration);
+        if(sv1->vid != sv2->vid)
+            return 0;
+        if(sv1->portMapCnt != sv2->portMapCnt)
+            return 0;
+        if(sv1->portMap != sv2->portMap)
+            for(i = 0; i < sv1->portMapCnt; i++)
+                if(sv1->portMap[i] != sv2->portMap[i])
+                    return 0;
+        return 1;
+
+    case FDB_RuleType_DynamicFiltering:
+        df1 = &(r1->rule.dynamicFiltering);
+        df2 = &(r2->rule.dynamicFiltering);
+        for(i = 0; i < ETHERNET_MAC_LEN; i++)
+            if(df1->mac[i] != df2->mac[i])
+                return 0;
+        if(df1->vid != df2->vid)
+            return 0;
+        if(df1->portMap != df2->portMap)
+            return 0;
+        return 1;
+
+    case FDB_RuleType_MACAddressRegistration:
+        ma1 = &(r1->rule.macAddressRegistration);
+        ma2 = &(r2->rule.macAddressRegistration);
+        for(i = 0; i < ETHERNET_MAC_LEN; i++)
+            if(ma1->mac[i] != ma2->mac[i])
+                return 0;
+        if(ma1->addrType != ma2->addrType)
+            return 0;
+        if(ma1->vid != ma2->vid)
+            return 0;
+        if(ma1->portMapCnt != ma2->portMapCnt)
+            return 0;
+        if(ma1->portMap != ma2->portMap)
+            for(i = 0; i < ma1->portMapCnt; i++)
+                if(ma1->portMap[i] != ma2->portMap[i])
+                    return 0;
+        return 1;
+
+    case FDB_RuleType_DynamicVLANRegistration:
+        dv1 = &(r1->rule.dynamicVLANRegistration);
+        dv2 = &(r2->rule.dynamicVLANRegistration);
+        if(dv1->vid != dv2->vid)
+            return 0;
+        if(dv1->portMapCnt != dv2->portMapCnt)
+            return 0;
+        if(dv1->portMap != dv2->portMap)
+            for(i = 0; i < dv1->portMapCnt; i++)
+                if(dv1->portMap[i] != dv2->portMap[i])
+                    return 0;
+        return 1;
+
+    case FDB_RuleType_DynamicReservation:
+        dr1 = &(r1->rule.dynamicRegistration);
+        dr2 = &(r2->rule.dynamicRegistration);
+        for(i = 0; i < ETHERNET_MAC_LEN; i++)
+            if(dr1->mac[i] != dr2->mac[i])
+                return 0;
+        if(dr1->vid != dr2->vid)
+            return 0;
+        if(dr1->portMapCnt != dr2->portMapCnt)
+            return 0;
+        return 1;
+
+    default:
+        return 0;
+    }
+}
--- bridge/filtering_db.h
+++ bridge/filtering_db.h
@@ -10,16 +10,17 @@
 
 #include <inttypes.h>
 #include "headers/ethernet.h"
+#include "bridge_forwarding.h"
 
 
-enum FDB_EntryType
+enum FDB_RuleType
 {
-    FDB_EntryType_StaticFiltering,
-    FDB_EntryType_StaticVLANRegistraiton,
-    FDB_EntryType_DynamicFiltering,
-    FDB_EntryType_MACAddressRegistration,
-    FDB_EntryType_DynamicVLANRegistration,
-    FDB_EntryType_DynamicReservation
+    FDB_RuleType_StaticFiltering,
+    FDB_RuleType_StaticVLANRegistraiton,
+    FDB_RuleType_DynamicFiltering,
+    FDB_RuleType_MACAddressRegistration,
+    FDB_RuleType_DynamicVLANRegistration,
+    FDB_RuleType_DynamicReservation
 };
 
 enum FDB_AddressType
@@ -32,40 +33,72 @@ enum FDB_AddressType
     FDB_AddressType_Group
 };
 
+enum FDB_PortMapEntry
+{
+    FDP_PortMapEntry_Filter,
+    FDP_PortMapEntry_Forward,
+    FDP_PortMapEntry_Dynamic,
+};
+
 struct FDB_StaticFiltering
 {
     uint8_t mac[ETHERNET_MAC_LEN];
     enum FDB_AddressType addrType;
+    enum FDB_PortMapEntry *portMap;
+    uint32_t portMapCnt;
 };
 
 struct FDB_StaticVLANRegistration
 {
-
+    uint16_t vid;
+    enum FDB_PortMapEntry *portMap;
+    uint32_t portMapCnt;
+    /*
+     * Following mapping for portMap:
+     *  - Filter <-> Forbidden
+     *  - Normal <-> Dynamic
+     *  - Fixed  <-> Forward
+     */
 };
 
 struct FDB_DynamicFiltering
 {
-
+    uint8_t mac[ETHERNET_MAC_LEN];
+    uint16_t vid; // korrekt: fid
+    uint32_t portMap; // Port to forward to.
 };
 
 struct FDB_MACAddressRegistration
 {
-
+    uint8_t mac[ETHERNET_MAC_LEN];
+    enum FDB_AddressType addrType;
+    // not allowed: AllUnregIndividual
+    uint16_t vid;
+    enum FDB_PortMapEntry *portMap;
+    uint32_t portMapCnt;
+    // Dynamic is invalid for portMap.
 };
 
 struct FDB_DynamicVLANRegistration
 {
-
+    uint16_t vid;
+    enum FDB_PortMapEntry *portMap;
+    uint32_t portMapCnt;
+    // Dynamic is invalid for portMap.
 };
 
 struct FDB_DynamicReservation
 {
-
+    uint8_t mac[ETHERNET_MAC_LEN];
+    uint16_t vid;
+    enum FDB_PortMapEntry *portMap;
+    uint32_t portMapCnt;
+    // Dynamic is invalid for portMap.
 };
 
-struct FDB_entry
+struct FDB_rule
 {
-    enum FDB_EntryType type;
+    enum FDB_RuleType type;
     union
     {
         struct FDB_StaticFiltering staticFiltering;
@@ -74,8 +107,45 @@ struct FDB_entry
         struct FDB_MACAddressRegistration macAddressRegistration;
         struct FDB_DynamicVLANRegistration dynamicVLANRegistration;
         struct FDB_DynamicReservation dynamicRegistration;
-    } entry;
+    } rule;
 };
 
+struct FDB_state
+{
+    uint32_t portCnt;
+    struct BridgeForwarding_state *bridgeForwarding;
+
+    struct FDB_rule *rules;
+    uint32_t ruleCnt;
+    uint32_t ruleAllocCnt;
+    int rulesStaticallyAllocated;
+};
+
+/*
+ * Return values:
+ *             0: success
+ *            -1: pointer NULL
+ *
+ */
+int32_t FDB_init(struct FDB_state *state, struct BridgeForwarding_state *bridgeForwarding, uint32_t portCnt, struct FDB_rule *ruleMemory, uint32_t ruleAllocCnt);
+
+/*
+ * Return values:
+ *             0: success
+ *            -1: pointer NULL
+ *            -2: rule invalid
+ *            -3: no memory left
+ */
+int32_t FDB_addRule(struct FDB_state *state, struct FDB_rule *rule);
+/*
+ * Return values:
+ *             0: success
+ *            -1: pointer NULL
+ *            -2: rule not found
+ */
+int32_t FDB_delRule(struct FDB_state *state, struct FDB_rule *rule);
+
+uint32_t FDB_getRuleCnt(struct FDB_state *state);
+struct FDB_rule *FDB_getRuleByIdx(struct FDB_state *state, uint32_t idx);
 
 #endif /* FILTERING_DB_H_ */
